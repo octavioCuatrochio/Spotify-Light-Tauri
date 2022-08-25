4f17fe712937ff7b5a067e543fa4a433b339f443
Octavio <octaviocuatrochio45545@gmail.com>
2022-08-25 06:44:54 -0300

    only import missing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,29 +1,33 @@
-#![cfg_attr(all(not(debug_assertions), target_os = "windows"), windows_subsystem = "windows")]
-
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 
 // the payload type must implement `Serialize` and `Clone`.
 #[derive(Clone, serde::Serialize)]
 struct Payload {
-    message: String,
+    url: String,
 }
 
 fn main() {
-    tauri::Builder
-        ::default()
+    tauri::Builder::default()
         .on_page_load(|window, payload| {
             let url = String::from(payload.url());
             let pos = url.find("#");
 
-            if pos != None {
-                window.emit("event-name", Payload { message: "Tauri is awesome!".into() }).unwrap();
-                println!("{}", payload.url());
-            }
+            let window_ = window.clone();
+            let _id = window.listen("frontend-loaded", move |event| {
+                println!("got window event-name with payload {:?}", event.payload());
+
+                if pos != None {
+                    println!("{}", payload.url());
+                    window_.emit("get-token", Payload { url: payload.url().into() }).unwrap();
+                } else {
+                    println!("{}", "No es url valida");
+                }
+            });
         })
-        .invoke_handler(tauri::generate_handler![greet])
+        // .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 const { invoke } = window.__TAURI__.tauri;
-
+const { once, emit, listen } = window.__TAURI__.event;
 
 let player;
 let img;
@@ -10,8 +10,9 @@ let prev_btn;
 let next_btn;
 let vol;
 
-let play_icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z"/></svg>`;
+let token_g;
 
+let play_icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M361 215C375.3 223.8 384 239.3 384 256C384 272.7 375.3 288.2 361 296.1L73.03 472.1C58.21 482 39.66 482.4 24.52 473.9C9.377 465.4 0 449.4 0 432V80C0 62.64 9.377 46.63 24.52 38.13C39.66 29.64 58.21 29.99 73.03 39.04L361 215z"/></svg>`;
 let pause_icon = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M272 63.1l-32 0c-26.51 0-48 21.49-48 47.1v288c0 26.51 21.49 48 48 48L272 448c26.51 0 48-21.49 48-48v-288C320 85.49 298.5 63.1 272 63.1zM80 63.1l-32 0c-26.51 0-48 21.49-48 48v288C0 426.5 21.49 448 48 448l32 0c26.51 0 48-21.49 48-48v-288C128 85.49 106.5 63.1 80 63.1z"/></svg>`;
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -21,47 +22,100 @@ window.addEventListener("DOMContentLoaded", () => {
   prev_btn = document.querySelector("#prev");
   next_btn = document.querySelector("#next");
   vol = document.querySelector("#vol");
-  console.log(window.__TAURI__.tauri);
+
+  setTimeout(() => {
+    emit("frontend-loaded", { ready: true });
+  }, 500);
+
+  once("get-token", async (event) => {
+    token_g = getToken(event.payload.url);
+    console.log(token_g);
+
+    (async () => {
+      const { Player } = await waitForSpotifyWebPlaybackSDKToLoad();
+      console.log("The Web Playback SDK has loaded.");
+
+      player = new Player({
+        name: "Web Playback SDK",
+        volume: 1.0,
+        getOAuthToken: (callback) => {
+          callback(token_g);
+        },
+      });
+
+      player.connect().then((connected) => {
+        if (connected) {
+          console.log("Connection was successful");
+        }
+      });
+    })();
+  });
 });
 
-window.onSpotifyWebPlaybackSDKReady = () => {
-  const token =
-    "BQBAmh70KmSFdoISHWxZ5qGgo7tcysGk5TkApLc17LykgdD6Dgrf-TTusERHMqCaH6hSyY8_RlmMfcmIWUeDnLvDATmAEfTKYLlBoyO8Ni-cVJo1zxnoZfilRTArdoEjafKUMZT0A7ggt_UyzvGgYNOzVFpftcCCLjTRdE-f2kjT5syO1kq9BzjYre-tYQ12E5p4c9JdEBrygC0ESxpSGFA";
-  player = new Spotify.Player({
-    name: "Spotify Light Player",
-    getOAuthToken: (cb) => {
-      cb(token);
-    },
-    volume: 0.5,
+async function waitForSpotifyWebPlaybackSDKToLoad() {
+  return new Promise((resolve) => {
+    if (window.Spotify) {
+      resolve(window.Spotify);
+    } else {
+      window.onSpotifyWebPlaybackSDKReady = () => {
+        resolve(window.Spotify);
+      };
+    }
   });
+}
 
-  // Ready
-  player.addListener("ready", ({ device_id }) => {
-    console.log("Ready with Device ID", device_id);
-  });
+// window.onSpotifyWebPlaybackSDKReady = () => {
+//   player = new Spotify.Player({
+//     name: "Spotify Light Player",
+//     getOAuthToken: (cb) => {
+//       cb(token_g);
+//     },
+//     volume: 0.5,
+//   });
 
-  // Not Ready
-  player.addListener("not_ready", ({ device_id }) => {
-    console.log("Device ID has gone offline", device_id);
-  });
+//   // Ready
+//   player.addListener("ready", ({ device_id }) => {
+//     console.log("Ready with Device ID", device_id);
+//   });
 
-  player.addListener("initialization_error", ({ message }) => {
-    console.error(message);
-  });
+//   // Not Ready
+//   player.addListener("not_ready", ({ device_id }) => {
+//     console.log("Device ID has gone offline", device_id);
+//   });
 
-  player.addListener("authentication_error", ({ message }) => {
-    console.error(message);
-  });
+//   player.addListener("initialization_error", ({ message }) => {
+//     console.error(message);
+//   });
 
-  player.addListener("account_error", ({ message }) => {
-    console.error(message);
-  });
+//   player.addListener("authentication_error", ({ message }) => {
+//     console.error(message);
+//   });
 
-  player.addListener("player_state_changed", (state) => {
-    updateUI(state);
-  });
-  player.connect();
-};
+//   player.addListener("account_error", ({ message }) => {
+//     console.error(message);
+//   });
+
+//   player.addListener("player_state_changed", (state) => {
+//     updateUI(state);
+//   });
+//   player.connect();
+// };
+
+function getToken(url) {
+  let search = "token=";
+  let start = url.indexOf(search) + search.length;
+
+  let token = "";
+  let c = "";
+  let i = start;
+  while (c != "&") {
+    token += c;
+    c = url[i];
+    i++;
+  }
+
+  return token;
+}
 
 function updateUI(state) {
   img.src = state.track_window.current_track.album.images[0].url;
@@ -86,6 +140,10 @@ async function changeVol(vol) {
 
   player.setVolume(f_vol);
 }
+
+once("event-name", async (event) => {
+  console.log(event);
+});
 
 async function play() {
   player.togglePlay();
